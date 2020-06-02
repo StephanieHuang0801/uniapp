57860a82b80ebea1f7512447d70e5bd664731dab
ying0801 <1029476703@qq.com>
2020-06-02 22:22:47 +0800

    uni-app找图项目初始化

---
+++ src/pages/index2/index.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-06-02 21:40:55
+ * @LastEditTime: 2020-06-02 22:10:48
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \my-project\src\pages\index2\index.vue
+--> 
+<template>
+  <view class="content">
+    <text class="title">page2</text>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "page2"
+    };
+  },
+  onLoad() {},
+  methods: {}
+};
+</script>
+
+<style lang='scss'>
+.content {
+  .title {
+    font-size: 36rpx;
+    color: $uni-color-primary;
+  }
+}
+</style>
